a459fe98b3cb92be50921f92175dac3b952e775c
chencchao <82246730+chencchao@users.noreply.github.com>
2023-03-23 16:43:26 +0800

    1、重构CPU项目路径

---
+++ CPU/__init__.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+# -*- coding:utf-8 -*-
+"""
+@ModuleName:Ai-Link
+@User:chenc
+@Date:2023/3/22 10:42 
+"""
---
+++ CPU/dome_01.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+# -*- coding:utf-8 -*-
+"""
+@ModuleName:Ai-Link
+@User:chenc
+@Date:2023/3/22 10:42 
+"""
+
+import GPUtil
+a = GPUtil.getGPUs()
+print(a)
+#
+# print(a.temperature)
